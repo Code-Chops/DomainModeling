9d31ba2f8081fcfe565a37ff30ec579369c67db8
Max Bergman <maxbergman@outlook.com>
2022-08-03 11:38:43 +0200

    0.5.11: Made readableentitydictionary an entity with IId.

--- DomainModeling/ReadableEntityDictionary.cs
+++ DomainModeling/ReadableEntityDictionary.cs
@@ -5,8 +5,8 @@ using CodeChops.DomainDrivenDesign.DomainModeling.Identities;
 
 namespace CodeChops.DomainDrivenDesign.DomainModeling;
 
-public abstract record ReadableEntityDictionary<TId, TEntity> : ValueObject, IReadOnlyDictionary<TId, TEntity>
-	where TId : Id
+public abstract class ReadableEntityDictionary<TId, TEntity> : Entity, IReadOnlyDictionary<TId, TEntity>
+	where TId : IId
 	where TEntity : Entity
 {
 	// ReSharper disable once MemberCanBePrivate.Global
@@ -22,18 +22,18 @@ public abstract record ReadableEntityDictionary<TId, TEntity> : ValueObject, IRe
 	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 }
 
-public abstract record ReadableEntityDictionary<TEntity> : ValueObject, IReadOnlyDictionary<Id, TEntity>
+public abstract class ReadableEntityDictionary<TEntity> : Entity, IReadOnlyDictionary<IId, TEntity>
 	where TEntity : Entity
 {
 	// ReSharper disable once MemberCanBePrivate.Global
-	protected Dictionary<Id, TEntity> Dictionary { get; } = new();
+	protected Dictionary<IId, TEntity> Dictionary { get; } = new();
 
-	public TEntity this[Id id] => this.Dictionary.GetValueOrDefault(id) ?? throw ExceptionHelpers.KeyNotFoundException<ReadableEntityDictionary<TEntity>, Id>(id);
-	public IEnumerable<Id> Keys => this.Dictionary.Keys;
+	public TEntity this[IId id] => this.Dictionary.GetValueOrDefault(id) ?? throw ExceptionHelpers.KeyNotFoundException<ReadableEntityDictionary<TEntity>, IId>(id);
+	public IEnumerable<IId> Keys => this.Dictionary.Keys;
 	public IEnumerable<TEntity> Values => this.Dictionary.Values;
 	public int Count => this.Dictionary.Count;
-	public bool ContainsKey(Id key) => this.Dictionary.ContainsKey(key);
-	public IEnumerator<KeyValuePair<Id, TEntity>> GetEnumerator() => this.Dictionary.GetEnumerator();
-	public bool TryGetValue(Id key, [MaybeNullWhen(false)] out TEntity value) => this.Dictionary.TryGetValue(key, out value);
+	public bool ContainsKey(IId key) => this.Dictionary.ContainsKey(key);
+	public IEnumerator<KeyValuePair<IId, TEntity>> GetEnumerator() => this.Dictionary.GetEnumerator();
+	public bool TryGetValue(IId key, [MaybeNullWhen(false)] out TEntity value) => this.Dictionary.TryGetValue(key, out value);
 	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 }

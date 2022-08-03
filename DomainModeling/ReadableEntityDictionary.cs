20c244eaaec129ba1195f7ee628feb1824260c3c
Max Bergman <maxbergman@outlook.com>
2022-08-03 15:51:25 +0200

    0.5.15: Changed ReadableEntityDictionary so it has a strongly typed ID.

--- DomainModeling/ReadableEntityDictionary.cs
+++ DomainModeling/ReadableEntityDictionary.cs
@@ -5,18 +5,19 @@ using CodeChops.DomainDrivenDesign.DomainModeling.Identities;
 
 namespace CodeChops.DomainDrivenDesign.DomainModeling;
 
-public abstract class ReadableEntityDictionary<TEntity> : Entity, IReadOnlyDictionary<Id, TEntity>
+public abstract record ReadableEntityDictionary<TId, TEntity> : IValueObject, IReadOnlyDictionary<TId, TEntity>
+	where TId : IId
 	where TEntity : Entity
 {
 	// ReSharper disable once MemberCanBePrivate.Global
-	protected Dictionary<Id, TEntity> Dictionary { get; } = new();
+	protected Dictionary<TId, TEntity> Dictionary { get; } = new();
 
-	public TEntity this[Id id] => this.Dictionary.GetValueOrDefault(id) ?? throw ExceptionHelpers.KeyNotFoundException<ReadableEntityDictionary<TEntity>, Id>(id);
-	public IEnumerable<Id> Keys => this.Dictionary.Keys;
+	public TEntity this[TId id] => this.Dictionary.GetValueOrDefault(id) ?? throw ExceptionHelpers.KeyNotFoundException<ReadableEntityDictionary<TId, TEntity>, TId>(id);
+	public IEnumerable<TId> Keys => this.Dictionary.Keys;
 	public IEnumerable<TEntity> Values => this.Dictionary.Values;
 	public int Count => this.Dictionary.Count;
-	public bool ContainsKey(Id key) => this.Dictionary.ContainsKey(key);
-	public IEnumerator<KeyValuePair<Id, TEntity>> GetEnumerator() => this.Dictionary.GetEnumerator();
-	public bool TryGetValue(Id key, [MaybeNullWhen(false)] out TEntity value) => this.Dictionary.TryGetValue(key, out value);
+	public bool ContainsKey(TId key) => this.Dictionary.ContainsKey(key);
+	public IEnumerator<KeyValuePair<TId, TEntity>> GetEnumerator() => this.Dictionary.GetEnumerator();
+	public bool TryGetValue(TId key, [MaybeNullWhen(false)] out TEntity value) => this.Dictionary.TryGetValue(key, out value);
 	IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
 }

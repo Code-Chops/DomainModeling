cf5a59c09aa4db1e10911cf23f4c177690af3689
Max Bergman <maxbergman@outlook.com>
2022-08-04 14:48:35 +0200

    0.8.1: Re-added tuple id.

---
+++ DomainModeling/Identities/TupleId.cs
@@ -0,0 +1,7 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace CodeChops.DomainDrivenDesign.DomainModeling.Identities;
+
+public abstract record TupleId<TSelf, TTuple> : Id<TSelf, TTuple>
+	where TSelf : TupleId<TSelf, TTuple>, new()
+	where TTuple : IStructuralEquatable, IStructuralComparable, IComparable, ITuple, IEquatable<TTuple>, IComparable<TTuple>;

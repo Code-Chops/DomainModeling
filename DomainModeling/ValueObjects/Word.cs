17ae20a16f21525445736d20c38a22a7362d25c8
Max Bergman <maxbergman@outlook.com>
2022-08-20 13:50:19 +0200

    0.15.11: Added CaseConversion to Word.

--- DomainModeling/ValueObjects/Word.cs
+++ DomainModeling/ValueObjects/Word.cs
@@ -3,7 +3,7 @@
 namespace CodeChops.DomainDrivenDesign.DomainModeling.ValueObjects;
 
 /// <summary>
-/// A string of type Word.<see cref="Type"/> in Latin characters.
+/// A string of type Word.<see cref="Type"/> in Latin characters with case-sensitive comparison (culture invariant).
 /// </summary>
 public readonly record struct Word : IEnumerable<char>, IHasEmptyInstance<Word>
 {
@@ -18,7 +18,7 @@ public readonly record struct Word : IEnumerable<char>, IHasEmptyInstance<Word>
 	public static implicit operator string(Word word) => word.Value;
 	public static explicit operator Word(string value) => new(value);
 
-	public Word(string value, int? maxLength = 0, Type type = Type.AlphaNumericWithUnderscore, [CallerMemberName] string? callerName = null)
+	public Word(string value, int? maxLength = 0, Type type = Type.AlphaNumericWithUnderscore, CaseConversion caseConversion = CaseConversion.NoConversion, [CallerMemberName] string? callerName = null)
 	{
 		var invalidCharacters = type switch
 		{
@@ -26,13 +26,19 @@ public readonly record struct Word : IEnumerable<char>, IHasEmptyInstance<Word>
 			Type.AlphaWithUnderscore		=> Regex.IsMatch(value, "^[a-zA-Z_]+$",		RegexOptions.Compiled),
 			Type.AlphaNumeric				=> Regex.IsMatch(value, "^[a-zA-Z0-9]+$",	RegexOptions.Compiled),
 			Type.AlphaNumericWithUnderscore => Regex.IsMatch(value, "^[a-zA-Z0-9_]+$",	RegexOptions.Compiled),
-			_									=> throw new ArgumentOutOfRangeException(nameof(type), type, null)
+			_								=> throw new ArgumentOutOfRangeException(nameof(type), type, null)
 		};
 
 		if (invalidCharacters) throw new ArgumentException($"Invalid characters in {callerName} {type} {nameof(Word)} '{value}'.");
 		if (value.Length > maxLength) throw new ArgumentException($"{callerName} {type} {nameof(Word)} '{value}' is longer ({value.Length}) than {nameof(maxLength)} {maxLength}.");
-		
-		this.Value = value;
+
+		this.Value = caseConversion switch
+		{
+			CaseConversion.NoConversion	=> value,
+			CaseConversion.AllLowercase	=> value.ToLowerInvariant(),
+			CaseConversion.AllUppercase	=> value.ToUpperInvariant(),
+			_ => throw new ArgumentOutOfRangeException(nameof(caseConversion), type, null)
+		};
 	}
 
 	public IEnumerator<char> GetEnumerator() => this.Value.GetEnumerator();
@@ -45,5 +51,12 @@ public readonly record struct Word : IEnumerable<char>, IHasEmptyInstance<Word>
 		AlphaWithUnderscore,
 		AlphaNumeric,
 		AlphaNumericWithUnderscore,
+	}
+
+	public enum CaseConversion
+	{
+		NoConversion,
+		AllLowercase,
+		AllUppercase,
 	}
 }

e625073d75e0e341246a49d75f4bf151a73e76aa
Max Bergman <maxbergman@outlook.com>
2022-09-18 23:30:17 +0200

    0.23.4: Added support for option JsonSerializationOptions for SystemException.

--- DomainModeling/Exceptions/SystemException.cs
+++ DomainModeling/Exceptions/SystemException.cs
@@ -1,12 +1,13 @@
-﻿using CodeChops.DomainDrivenDesign.DomainModeling.DisplayString;
+﻿using System.Text.Json;
+using CodeChops.DomainDrivenDesign.DomainModeling.DisplayString;
 
 namespace CodeChops.DomainDrivenDesign.DomainModeling.Exceptions;
 
 public abstract class SystemException<TException, TParameter> : Exception 
 	where TException : SystemException<TException, TParameter>, ISystemException<TException, TParameter>
 {
-	protected SystemException(TParameter parameter, string? extraText = null)
-		: base(message: $"{TException.ErrorMessage}. Info: {DisplayStringHelper.ToDisplayString<object>(parameter, extraText: extraText)}")	
+	protected SystemException(TParameter parameter, string? extraText = null, JsonSerializerOptions? jsonSerializerOptions = null!)
+		: base(message: $"{TException.ErrorMessage}. Info: {DisplayStringHelper.ToDisplayString<object>(parameter, extraText: extraText, jsonSerializerOptions)}")	
 	{
 	}
 }

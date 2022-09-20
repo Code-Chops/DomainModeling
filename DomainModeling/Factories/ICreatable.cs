8823a0f81b8106b7879e1a1dd191747c99e467aa
Max Bergman <maxbergman@outlook.com>
2022-09-20 17:27:27 +0200

    0.25.6: Reversed generic parameters of ICreatable.

--- DomainModeling/Factories/ICreatable.cs
+++ DomainModeling/Factories/ICreatable.cs
@@ -4,7 +4,7 @@
 /// Has a factory to create objects of type <typeparamref name="T"/> with parameter <typeparamref name="TParameter"/>.
 /// </summary>
 /// <typeparam name="T">The type to be created.</typeparam>
-public interface ICreatable<in TParameter, out T>
+public interface ICreatable<out T, in TParameter>
 {
 	static abstract T Create(TParameter parameter);
 }
